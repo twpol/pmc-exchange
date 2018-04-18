b28539d455975a9e8ffd3063ef0dcf7f346c4ab9
James Ross <git@james-ross.co.uk>
2018-04-18 20:17:42 +0100

    feat: Add command line parsing and configuration file loading

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.IO;
+using CommandLine;
+using Microsoft.Extensions.Configuration;
 
-namespace pmc_exchange
+namespace pmc.exchange
 {
     class Program
     {
-        static void Main(string[] args)
+        class Options
         {
-            Console.WriteLine("Hello World!");
+            [Option(HelpText = "Display details of what the program is doing.")]
+            public bool Verbose { get; set; } = false;
+
+            [Option(HelpText = "Specify the configuration file to use (default: config.json).")]
+            public FileInfo Config { get; set; } = new FileInfo("config.json");
+        }
+
+        static int Main(string[] args)
+        {
+            return Parser.Default.ParseArguments<Options>(args)
+                .MapResult(
+                    options => Run(new ConfigurationBuilder()
+                        .AddJsonFile(options.Config.FullName, true)
+                        .Build(), options),
+                    _ => 1
+                );
+        }
+
+        static int Run(IConfigurationRoot configurationRoot, Options options)
+        {
+            return 0;
         }
     }
 }
